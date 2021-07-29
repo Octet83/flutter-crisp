955e537046e123ba1dc4bdad465e8459bb50b3de
Yann Levy <yann.levy88@gmail.com>
2021-07-30 08:49:33 +1100

    Update main.dart

--- lib/models/main.dart
+++ lib/models/main.dart
@@ -34,51 +34,34 @@ class CrispMain {
   /// Register a new user to start the chat
   void register({required CrispUser user}) {
     if (user.verificationCode != null)
-      appendScript("window.\$crisp.push([\"set\", \"user:email\", [\"" +
-          user.email +
-          "\", \"" +
-          user.verificationCode! +
-          "\"]])");
+      appendScript("window.\$crisp.push([\"set\", \"user:email\", [\"" + user.email + "\", \"" + user.verificationCode! + "\"]])");
     else
-      appendScript("window.\$crisp.push([\"set\", \"user:email\", [\"" +
-          user.email +
-          "\"]])");
+      appendScript("window.\$crisp.push([\"set\", \"user:email\", [\"" + user.email + "\"]])");
 
-    if (user.nickname != null)
-      appendScript("window.\$crisp.push([\"set\", \"user:nickname\", [\"" +
-          user.nickname! +
-          "\"]])");
+    if (user.nickname != null) appendScript("window.\$crisp.push([\"set\", \"user:nickname\", [\"" + user.nickname! + "\"]])");
 
-    if (user.avatar != null)
-      appendScript("window.\$crisp.push([\"set\", \"user:avatar\", [\"" +
-          user.avatar! +
-          "\"]])");
+    if (user.avatar != null) appendScript("window.\$crisp.push([\"set\", \"user:avatar\", [\"" + user.avatar! + "\"]])");
 
-    if (user.phone != null)
-      appendScript("window.\$crisp.push([\"set\", \"user:phone\", [\"" +
-          user.phone! +
-          "\"]])");
+    if (user.phone != null) appendScript("window.\$crisp.push([\"set\", \"user:phone\", [\"" + user.phone! + "\"]])");
 
+    appendScript("window.CRISP_RUNTIME_CONFIG = {lock_maximized: true,lock_full_view: true,cross_origin_cookies : true}");
     this.user = user;
   }
 
   setMessage(String text) {
-    appendScript(
-        "window.\$crisp.push([\"set\", \"message:text\", [\"$text\"]])");
+    appendScript("window.\$crisp.push([\"set\", \"message:text\", [\"$text\"]])");
   }
 
   setSegments(List<String> segments) {
     if (segments.isEmpty) return;
 
-    segments.forEach((value) => appendScript(
-        'window.\$crisp.push(["set", "session:segments", [["$value"]]]);'));
+    segments.forEach((value) => appendScript('window.\$crisp.push(["set", "session:segments", [["$value"]]]);'));
   }
 
   setSessionData(Map<String, String> sessionData) {
     if (sessionData.isEmpty) return;
 
-    sessionData.forEach((key, value) => appendScript(
-        'window.\$crisp.push(["set", "session:data", ["$key", "$value"]]);'));
+    sessionData.forEach((key, value) => appendScript('window.\$crisp.push(["set", "session:data", ["$key", "$value"]]);'));
   }
 
   void appendScript(String script) {
